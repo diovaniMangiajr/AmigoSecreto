0eb9af992639f25a9f6a93e75cf22717cec8852b
diovaniMangiajr <diovanimacieljr@gmail.com>
2025-03-08 17:14:33 -0300

    Adicionar amigos foi adicionada porém com um bug.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo () {
+    let entrada = document.getElementById('amigo');
+    let texto = entrada.value;
+
+    if (texto != "") {
+        nomes.push(texto);
+        entrada = "";
+
+        console.log(nomes);
+    } else {
+        alert('Digite um nome para adicionar');
+    }
+}
+
